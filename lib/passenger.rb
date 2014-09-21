3320673e6694efb2e442a97c7fc1883b5ca63a39
Victoria Barr <vb329@georgetown.edu>
2014-09-21 12:28:32 +0100

    Passenger initialized with credit, pays 2 credits to ride, and cannot enter station w/o at least 2 GBP credit

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -1,12 +1,21 @@
 class Passenger
 
-	def initialize
+	attr_accessor :credit
+
+	def initialize(credit=20)
 		@in_station = false
+		@credit = credit
 	end
 
+	def pay_fare
+		@credit = @credit - 2
+	end
+	
 	def enter(station)
+		raise 'You do not have enough credit on your card. Please top up your card to ride.' if @credit < 2
 		station.touch_in(self)
 		@in_station = true
+		pay_fare
 	end
 
 	def exit(station)
@@ -22,5 +31,9 @@ class Passenger
 	def get_off(coach, station)
 		coach.drop_off(self)
 		enter(station)		
+	end
+
+	def credit
+		@credit
 	end
 end
--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -29,7 +29,6 @@ let(:station) {Station.new}
 		coach = double :coach
 		allow(coach).to receive(:pick_up)
 		expect(lambda {passenger.board(station, coach)}).to raise_error(RuntimeError)
-
 	end
 
 	it "should be able to get off the coach" do
@@ -37,6 +36,24 @@ let(:station) {Station.new}
 		expect(coach).to receive(:drop_off)
 		passenger.get_off(coach, station)
 		expect(station.passenger_count).to eq(1)
+	end
+
+	it "should start off with 20 GBP credit" do
+		expect(passenger.credit).to eq(20)
+	end
+
+	it "should pay 2 GBP to ride the tube" do
+		passenger.enter(station)
+		expect(passenger.credit).to eq(18)
+		passenger.exit(station)
+		passenger.enter(station)
+		expect(passenger.credit).to eq(16)
+	end
+
+	it "should not be able to ride without enough credit" do 
+		passenger = Passenger.new(0)
+		expect(passenger.credit).to eq(0)
+		expect(lambda {passenger.enter(station)}).to raise_error(RuntimeError)
 	end
 
 end
