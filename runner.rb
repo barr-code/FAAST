3a73d976e658b9df762f1a3ceeb4169f1f615833
Victoria Barr <vb329@georgetown.edu>
2014-09-22 16:30:59 +0100

    Refactored Train tests; included runner file

---
+++ runner.rb
@@ -0,0 +1,4 @@
+require './lib/passenger.rb'
+require './lib/coach.rb'
+require './lib/station.rb'
+require './lib/train.rb'
--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -1,12 +1,10 @@
 require 'train'
-require 'station'
-require 'coach'
 
 describe Train do
 
 	let(:train) {Train.new}
-	let(:station) {Station.new}
-	let(:coach) {Coach.new}
+	let(:station) {double :station}
+	let(:coach) {double :coach}
 
 	it "should be have a default capacity of 10 coaches" do
 		expect(train.capacity).to eq(10)
@@ -18,20 +16,17 @@ describe Train do
 	end
 
 	it "should be able to arrive at station" do
+		expect(station).to receive(:accept)
 		train.load(coach)
 		train.travel(nil, station)
-		expect(station.train_count).to eq(1)
 	end
 
 	it "should move from one station to another" do 
 		station1, station2 = Station.new, Station.new
 		train.load(coach)
 		train.travel(nil, station1)
-		expect(station2.train_count).to eq(0)
-		expect(station1.train_count).to eq(1)
-		train.travel(station1, station2)
-		expect(station1.train_count).to eq(0)
-		expect(station2.train_count).to eq(1)
+		expect{train.travel(station1, station2)}.to change{station1.train_count}.by(-1)
+		expect{train.travel(station1, station2)}.to change{station2.train_count}.by(1)
 	end
 
 	it "should not transfer more coaches that its capacity" do
